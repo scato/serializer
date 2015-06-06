6d6287111348e781708e68c1a8162180313845c9
Scato Eggen <scato.eggen@gmail.com>
2015-06-06 22:19:55 +0200

    Add JSON visitors and encoding

---
+++ spec/Scato/Serializer/Json/FromJsonVisitorSpec.php
@@ -0,0 +1,169 @@
+<?php
+
+namespace spec\Scato\Serializer\Json;
+
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+use Scato\Serializer\Common\PublicAccessor;
+use Scato\Serializer\Common\TypeProviderInterface;
+
+class FromJsonVisitorSpec extends ObjectBehavior
+{
+    function let(TypeProviderInterface $typeProvider)
+    {
+        $this->beConstructedWith(new PublicAccessor(), $typeProvider);
+    }
+
+    function it_should_be_a_typed_visitor()
+    {
+        $this->shouldHaveType('Scato\Serializer\Core\TypedVisitorInterface');
+    }
+
+    function it_should_handle_an_object_with_a_string() {
+        $object = new Person(1, "Bryon Hetrick", true);
+
+        $this->visitType(get_class($object));
+        $this->visitObjectStart('stdClass');
+        $this->visitProperties();
+        $this->visitObjectEnd('stdClass');
+
+        $this->getResult()->shouldBeLike($object);
+    }
+
+    function it_should_handle_an_object_with_an_object(TypeProviderInterface $typeProvider) {
+        $object = new Person(1, "Bryon Hetrick", true);
+        $object->address = new Address('Dam', '1', 'Amsterdam');
+
+        $typeOfPerson = Argument::type('spec\Scato\Serializer\Json\Person');
+
+        $typeProvider->getType($typeOfPerson, 'address')->willReturn('spec\Scato\Serializer\Json\Address');
+        $typeProvider->getType(Argument::any(), Argument::any())->willReturn(null);
+
+        $this->visitType(get_class($object));
+        $this->visitObjectStart('stdClass');
+        $this->visitProperties();
+        $this->visitAddress();
+        $this->visitObjectEnd('stdClass');
+
+        $this->getResult()->shouldBeLike($object);
+    }
+
+    function it_should_handle_an_object_with_an_array(TypeProviderInterface $typeProvider) {
+        $object = new Person(1, "Bryon Hetrick", true);
+        $object->phoneNumbers[] = new PhoneNumber('Home', '0201234567');
+        $object->phoneNumbers[] = new PhoneNumber('Mobile', '0612345678');
+
+        $typeOfPerson = Argument::type('spec\Scato\Serializer\Json\Person');
+
+        $typeProvider->getType($typeOfPerson, 'phoneNumbers')->willReturn('spec\Scato\Serializer\Json\PhoneNumber[]');
+        $typeProvider->getType(Argument::any(), Argument::any())->willReturn(null);
+
+        $this->visitType(get_class($object));
+        $this->visitObjectStart('stdClass');
+        $this->visitProperties();
+        $this->visitPhoneNumbers();
+        $this->visitObjectEnd('stdClass');
+
+        $this->getResult()->shouldBeLike($object);
+    }
+
+    private function visitProperties()
+    {
+        $this->visitPropertyStart('personId');
+        $this->visitNumber(1);
+        $this->visitPropertyEnd('personId');
+        $this->visitPropertyStart('name');
+        $this->visitString('Bryon Hetrick');
+        $this->visitPropertyEnd('name');
+        $this->visitPropertyStart('registered');
+        $this->visitBoolean(true);
+        $this->visitPropertyEnd('registered');
+    }
+
+    private function visitAddress()
+    {
+        $this->visitPropertyStart('address');
+        $this->visitObjectStart('stdClass');
+        $this->visitPropertyStart('street');
+        $this->visitString('Dam');
+        $this->visitPropertyEnd('street');
+        $this->visitPropertyStart('number');
+        $this->visitString('1');
+        $this->visitPropertyEnd('number');
+        $this->visitPropertyStart('city');
+        $this->visitString('Amsterdam');
+        $this->visitPropertyEnd('city');
+        $this->visitObjectEnd('stdClass');
+        $this->visitPropertyEnd('address');
+    }
+
+    private function visitPhoneNumbers()
+    {
+        $this->visitPropertyStart('phoneNumbers');
+        $this->visitArrayStart();
+        $this->visitElementStart(0);
+        $this->visitObjectStart('stdClass');
+        $this->visitPropertyStart('name');
+        $this->visitString('Home');
+        $this->visitPropertyEnd('name');
+        $this->visitPropertyStart('number');
+        $this->visitString('0201234567');
+        $this->visitPropertyEnd('number');
+        $this->visitObjectEnd('stdClass');
+        $this->visitElementEnd(0);
+        $this->visitElementStart(1);
+        $this->visitObjectStart('stdClass');
+        $this->visitPropertyStart('name');
+        $this->visitString('Mobile');
+        $this->visitPropertyEnd('name');
+        $this->visitPropertyStart('number');
+        $this->visitString('0612345678');
+        $this->visitPropertyEnd('number');
+        $this->visitObjectEnd('stdClass');
+        $this->visitElementEnd(1);
+        $this->visitArrayEnd();
+        $this->visitPropertyEnd('phoneNumbers');
+    }
+}
+
+class Person
+{
+    public $personId;
+    public $name;
+    public $registered;
+    public $address;
+    public $phoneNumbers = array();
+
+    public function __construct($personId, $name, $registered)
+    {
+        $this->personId = $personId;
+        $this->name = $name;
+        $this->registered = $registered;
+    }
+}
+
+class Address
+{
+    public $street;
+    public $number;
+    public $city;
+
+    public function __construct($street, $number, $city)
+    {
+        $this->street = $street;
+        $this->number = $number;
+        $this->city = $city;
+    }
+}
+
+class PhoneNumber
+{
+    public $name;
+    public $number;
+
+    public function __construct($name, $number)
+    {
+        $this->name = $name;
+        $this->number = $number;
+    }
+}
---
+++ spec/Scato/Serializer/Json/ToJsonVisitorSpec.php
@@ -0,0 +1,122 @@
+<?php
+
+namespace spec\Scato\Serializer\Json;
+
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+use Scato\Serializer\Common\PublicAccessor;
+use stdClass;
+
+class ToJsonVisitorSpec extends ObjectBehavior
+{
+    function let()
+    {
+        $this->beConstructedWith(new PublicAccessor());
+    }
+
+    function it_should_be_a_visitor()
+    {
+        $this->shouldHaveType('Scato\Serializer\Core\VisitorInterface');
+    }
+
+    function it_should_handle_an_empty_array()
+    {
+        $this->visitArrayStart();
+        $this->visitArrayEnd();
+
+        $this->getResult()->shouldBe(array());
+    }
+
+    function it_should_handle_an_array_with_a_string()
+    {
+        $this->visitArrayStart();
+        $this->visitElementStart('foo');
+        $this->visitString('bar');
+        $this->visitElementEnd('foo');
+        $this->visitArrayEnd();
+
+        $this->getResult()->shouldBe(array('foo' => 'bar'));
+    }
+
+    function it_should_handle_an_array_with_an_array()
+    {
+        $this->visitArrayStart();
+        $this->visitElementStart(0);
+        $this->visitArrayStart();
+        $this->visitElementStart(0);
+        $this->visitString('foo');
+        $this->visitElementEnd(0);
+        $this->visitArrayEnd();
+        $this->visitElementEnd(0);
+        $this->visitArrayEnd();
+
+        $this->getResult()->shouldBe(array(array('foo')));
+    }
+
+    function it_should_handle_an_empty_object() {
+        $object = new stdClass();
+
+        $this->visitObjectStart('ExampleObject');
+        $this->visitObjectEnd('ExampleObject');
+
+        $this->getResult()->shouldBeLike($object);
+    }
+
+    function it_should_handle_an_object_with_a_string() {
+        $object = new stdClass();
+        $object->foo = 'bar';
+
+        $this->visitObjectStart('ExampleObject');
+        $this->visitPropertyStart('foo');
+        $this->visitString('bar');
+        $this->visitPropertyEnd('foo');
+        $this->visitObjectEnd('ExampleObject');
+
+        $this->getResult()->shouldBeLike($object);
+    }
+
+    function it_should_handle_an_object_with_an_array() {
+        $object = new stdClass();
+        $object->foo = array('bar');
+
+        $this->visitObjectStart('ExampleObject');
+        $this->visitPropertyStart('foo');
+        $this->visitArrayStart();
+        $this->visitElementStart(0);
+        $this->visitString('bar');
+        $this->visitElementEnd(0);
+        $this->visitArrayEnd();
+        $this->visitPropertyEnd('foo');
+        $this->visitObjectEnd('ExampleObject');
+
+        $this->getResult()->shouldBeLike($object);
+    }
+
+    function it_should_handle_a_string()
+    {
+        $this->visitString('foobar');
+
+        $this->getResult()->shouldBe('foobar');
+    }
+
+    function it_should_handle_null()
+    {
+        $this->visitNull();
+
+        $this->getResult()->shouldBe(null);
+    }
+
+    function it_should_handle_a_number()
+    {
+        $this->visitNumber(1);
+
+        $this->getResult()->shouldBe(1);
+    }
+
+    function it_should_handle_a_boolean()
+    {
+        $this->visitBoolean(true);
+
+        $this->getResult()->shouldBe(true);
+    }
+}
