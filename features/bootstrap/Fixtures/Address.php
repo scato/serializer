1839a1b9b93e0b01eed9adcab15b9d3afca6209c
Scato Eggen <seggen@ibuildings.nl>
2015-06-06 22:16:05 +0200

    Add feature for serializing and deserializing JSON

---
+++ features/bootstrap/Fixtures/Address.php
@@ -0,0 +1,23 @@
+<?php
+
+
+namespace Fixtures;
+
+
+class Address
+{
+    /**
+     * @var string
+     */
+    public $street;
+
+    /**
+     * @var string
+     */
+    public $number;
+
+    /**
+     * @var string
+     */
+    public $city;
+}
---
+++ features/bootstrap/Fixtures/PhoneNumber.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace Fixtures;
+
+
+class PhoneNumber
+{
+    /**
+     * @var string
+     */
+    public $name;
+
+    /**
+     * @var string
+     */
+    public $number;
+}
