52ac6e8605e2d325be4e9306b0fde9ce5c21de2c
Scato Eggen <seggen@ibuildings.nl>
2015-06-07 13:42:58 +0200

    Refactor visitors to increase readability

--- src/Scato/Serializer/Url/FromUrlVisitor.php
+++ src/Scato/Serializer/Url/FromUrlVisitor.php
@@ -6,39 +6,15 @@ use Scato\Serializer\Common\MapToObjectVisitor;
 
 class FromUrlVisitor extends MapToObjectVisitor
 {
-    public function visitArrayStart()
-    {
-        if ($this->inArray(1)) {
-            parent::visitArrayStart();
-        } else {
-            parent::visitObjectStart('array');
-        }
-    }
-
     public function visitArrayEnd()
     {
-        if ($this->inArray(1)) {
+        $type = $this->peekType(1);
+        $inArray = $type === null || $type === 'array' || preg_match('/\\[\\]$/', $type);
+
+        if ($inArray) {
             parent::visitArrayEnd();
         } else {
             parent::visitObjectEnd('array');
-        }
-    }
-
-    public function visitElementStart($key)
-    {
-        if ($this->inArray(1)) {
-            parent::visitElementStart($key);
-        } else {
-            parent::visitPropertyStart($key);
-        }
-    }
-
-    public function visitElementEnd($key)
-    {
-        if ($this->inArray(2)) {
-            parent::visitElementEnd($key);
-        } else {
-            parent::visitPropertyEnd($key);
         }
     }
 
@@ -55,12 +31,5 @@ class FromUrlVisitor extends MapToObjectVisitor
         } else {
             parent::visitString($value);
         }
-    }
-
-    private function inArray($count)
-    {
-        $type = $this->peekType($count);
-
-        return $type === null || $type === 'array' || preg_match('/\\[\\]$/', $type);
     }
 }
