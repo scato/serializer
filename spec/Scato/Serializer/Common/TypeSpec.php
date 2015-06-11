2fc562c122d764ecad7c8cbfca70b3cfc2e7ec5b
Scato Eggen <seggen@ibuildings.nl>
2015-06-11 14:59:29 +0200

    Introduce value object for types

---
+++ spec/Scato/Serializer/Common/TypeSpec.php
@@ -0,0 +1,129 @@
+<?php
+
+namespace spec\Scato\Serializer\Common;
+
+use LogicException;
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class TypeSpec extends ObjectBehavior
+{
+    function it_should_be_converted_back_to_a_string()
+    {
+        $this->beConstructedThrough('fromString', array('Foo'));
+
+        $this->toString()->shouldBe('Foo');
+    }
+
+    function it_should_be_mixed_by_default()
+    {
+        $this->beConstructedThrough('fromString', array());
+
+        $this->toString()->shouldBe('mixed');
+    }
+
+    function it_should_use_mixed_for_unknown_types()
+    {
+        $this->beConstructedThrough('fromString', array(null));
+
+        $this->toString()->shouldBe('mixed');
+    }
+
+    function it_should_recognize_class_types()
+    {
+        $this->beConstructedThrough('fromString', array('Foo'));
+
+        $this->isClass()->shouldBe(true);
+        $this->isArray()->shouldBe(false);
+        $this->isInteger()->shouldBe(false);
+        $this->isFloat()->shouldBe(false);
+        $this->isBoolean()->shouldBe(false);
+
+        $this->getArrayType()->toString()->shouldBe('Foo[]');
+        $this->shouldThrow(new LogicException("Type 'Foo' is not an array type"))->duringGetElementType();
+    }
+
+    function it_should_recognize_integer_types()
+    {
+        $this->beConstructedThrough('fromString', array('int'));
+
+        $this->isClass()->shouldBe(false);
+        $this->isArray()->shouldBe(false);
+        $this->isInteger()->shouldBe(true);
+        $this->isFloat()->shouldBe(false);
+        $this->isBoolean()->shouldBe(false);
+
+        $this->getArrayType()->toString()->shouldBe('int[]');
+        $this->shouldThrow(new LogicException("Type 'int' is not an array type"))->duringGetElementType();
+    }
+
+    function it_should_recognize_float_types()
+    {
+        $this->beConstructedThrough('fromString', array('float'));
+
+        $this->isClass()->shouldBe(false);
+        $this->isArray()->shouldBe(false);
+        $this->isInteger()->shouldBe(false);
+        $this->isFloat()->shouldBe(true);
+        $this->isBoolean()->shouldBe(false);
+
+        $this->getArrayType()->toString()->shouldBe('float[]');
+        $this->shouldThrow(new LogicException("Type 'float' is not an array type"))->duringGetElementType();
+    }
+
+    function it_should_recognize_boolean_types()
+    {
+        $this->beConstructedThrough('fromString', array('bool'));
+
+        $this->isClass()->shouldBe(false);
+        $this->isArray()->shouldBe(false);
+        $this->isInteger()->shouldBe(false);
+        $this->isFloat()->shouldBe(false);
+        $this->isBoolean()->shouldBe(true);
+
+        $this->getArrayType()->toString()->shouldBe('bool[]');
+        $this->shouldThrow(new LogicException("Type 'bool' is not an array type"))->duringGetElementType();
+    }
+
+    function it_should_recognize_the_primitive_array_type()
+    {
+        $this->beConstructedThrough('fromString', array('array'));
+
+        $this->isClass()->shouldBe(false);
+        $this->isArray()->shouldBe(true);
+        $this->isInteger()->shouldBe(false);
+        $this->isFloat()->shouldBe(false);
+        $this->isBoolean()->shouldBe(false);
+
+        $this->getArrayType()->toString()->shouldBe('array[]');
+        $this->getElementType()->toString()->shouldBe('mixed');
+    }
+
+    function it_should_recognize_special_types()
+    {
+        $this->beConstructedThrough('fromString', array('mixed'));
+
+        $this->isClass()->shouldBe(false);
+        $this->isArray()->shouldBe(false);
+        $this->isInteger()->shouldBe(false);
+        $this->isFloat()->shouldBe(false);
+        $this->isBoolean()->shouldBe(false);
+
+        $this->getArrayType()->toString()->shouldBe('array');
+        $this->shouldThrow(new LogicException("Type 'mixed' is not an array type"))->duringGetElementType();
+    }
+
+    function it_should_recognize_array_types()
+    {
+        $this->beConstructedThrough('fromString', array('Foo[]'));
+
+        $this->isClass()->shouldBe(false);
+        $this->isArray()->shouldBe(true);
+        $this->isInteger()->shouldBe(false);
+        $this->isFloat()->shouldBe(false);
+        $this->isBoolean()->shouldBe(false);
+
+        $this->getArrayType()->toString()->shouldBe('Foo[][]');
+        $this->getElementType()->toString()->shouldBe('Foo');
+    }
+}
---
+++ src/Scato/Serializer/Common/Type.php
@@ -0,0 +1,132 @@
+<?php
+
+namespace Scato\Serializer\Common;
+
+use LogicException;
+
+class Type
+{
+    private $string;
+
+    private static $primitiveTypes = array(
+        'string',
+        'int',
+        'integer',
+        'float',
+        'bool',
+        'boolean',
+        'array',
+        'resource',
+        'null',
+        'callable'
+    );
+
+    private static $specialTypes = array(
+        'mixed',
+        'void',
+        'object',
+        'false',
+        'true',
+        'self',
+        'static',
+        '$this'
+    );
+
+    private function __construct($string)
+    {
+        $this->string = $string;
+    }
+
+    public static function fromString($string = null)
+    {
+        if ($string === null) {
+            $string = 'mixed';
+        }
+
+        return new self($string);
+    }
+
+    public function toString()
+    {
+        return $this->string;
+    }
+
+    public function isClass()
+    {
+        if (in_array($this->string, self::$primitiveTypes)) {
+            return false;
+        }
+
+        if (in_array($this->string, self::$specialTypes)) {
+            return false;
+        }
+
+        if (preg_match('/\\[\\]$/', $this->string)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public function isArray()
+    {
+        if ($this->string === 'array') {
+            return true;
+        }
+
+        if (preg_match('/\\[\\]$/', $this->string)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public function isInteger()
+    {
+        if (in_array($this->string, array('int', 'integer'))) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public function isFloat()
+    {
+        if (in_array($this->string, array('float'))) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public function isBoolean()
+    {
+        if (in_array($this->string, array('bool', 'boolean'))) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public function getElementType()
+    {
+        if ($this->string === 'array') {
+            return self::fromString('mixed');
+        }
+
+        if (preg_match('/\\[\\]$/', $this->string)) {
+            return self::fromString(preg_replace('/\\[\\]$/', '', $this->string));
+        }
+
+        throw new LogicException("Type '{$this->string}' is not an array type");
+    }
+
+    public function getArrayType()
+    {
+        if ($this->string === 'mixed') {
+            return self::fromString('array');
+        }
+
+        return self::fromString($this->string . '[]');
+    }
+}
